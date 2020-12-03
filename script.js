f79834cfe6ade783661ede5ca7fd7878744db5ad
Adam Arras <adamarras@gmail.com>
2020-12-02 22:06:37 -0600

    made some changes to validation

--- script.js
+++ script.js
@@ -21,10 +21,17 @@ window.addEventListener("load", function () {
       let copilotNameInput = document.querySelector("input[name=copilotName");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]")
       let cargoMassInput = document.querySelector("input[name=cargoMass]")
+      //alert(typeof pilotNameInput.value);
       if(pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
          alert("All fields are required.");
-      } else {
+      } else if(isNaN(pilotNameInput.value) == false || isNaN(copilotNameInput.value) == false) {
+         alert("Name fields cannot be numbers.");
+      } else if (isNaN(fuelLevelInput.value) == true) {
+         alert("Fuel and Cargo fields must be numbers.");
+         } else {
          document.getElementById("faultyItems").style.visibility = "hidden";
+         document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch"
+         document.getElementById("cargoStatus").innerHTML = "Cargo mass low enough for launch";
          document.getElementById("pilotStatus").innerHTML = `${pilotNameInput.value} is ready`;
          document.getElementById("copilotStatus").innerHTML = `${copilotNameInput.value} is ready`;
          document.getElementById("launchStatus").style.color = "green";
